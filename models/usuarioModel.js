8801aa63da2efef15dececa5edd97cf622ec7442
Inacio983 <inacio-sbellaguarda@educar.rs.gov.br>
2023-11-08 21:40:29 -0300

    first commit

--- models/usuarioModel.js
+++ models/usuarioModel.js
@@ -7,7 +7,7 @@ class Usuario {
 	static async autenticar(email, senha){
 		const md5 = require('md5');
 		const Database= require('./Database');
-		let sql=`SELECT * FROM usuario WHERE email ='${email}' AND senha ='${md5(senha)}'`;
+		let sql=`SELECT * FROM usuario WHERE email ='${email}' AND senha ='${md5(senha)}';`;
 		console.log(sql);
 		return await Database.query(sql);
 		
